f3b9a99473350a9eae57b4b4fa12343d23b28285
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-02-21 12:25:10 +0100

    solved 7 challenges

---
+++ Day1/challenges_day1.js
@@ -0,0 +1,13 @@
+let emptyArray = [];
+
+let secondArray = ["hello",1,5,"code","phone","vs code"];
+
+console.log(secondArray.length); // to show the length of the arrey: second array
+
+console.log(secondArray[0]); // to show the first item in the table
+
+console.log(secondArray[secondArray.length-1]);//to show the last item in the table
+
+console.log(secondArray[Math.ceil(secondArray.length-1)]);
+
+let mixedDataTypes= [15,"variable",1500,{name:"morocco",population:4000000,weather:"mostly even","Youssouf"}]
