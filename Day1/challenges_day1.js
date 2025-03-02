cc42cf2e280a153f839d6d1f932a8f2fb9167091
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-03-02 15:13:02 +0000

    addedsolver two other challenges

--- Day1/challenges_day1.js
+++ Day1/challenges_day1.js
@@ -10,4 +10,45 @@ console.log(secondArray[secondArray.length-1]);//to show the last item in the ta
 
 console.log(secondArray[Math.ceil(secondArray.length-1)]);
 
-let mixedDataTypes= [15,"variable",1500,{name:"morocco",population:4000000,weather:"mostly even","Youssouf"}]
+let mixedDataTypes= [15,"variable",1500,{name:"morocco",population:4000000,weather:"mostly even",ville:"Youssoufia"}];
+
+let itCompanies = [];
+itCompanies.push("Facebook","Google","Microsoft","Apple","Apple","Oracle","Amazon");
+
+//Check if a company exitsts in an array---------------------------------------------------------------------------------
+
+function doesExist(array, company){
+    if (array.includes(company)){
+        console.log("the company: "+ company + " exists in the table.");
+    }else{
+        console.log("the company: "+ company + " does not exists in the table.");
+
+    }
+}
+
+doesExist(itCompanies,"Facebook");
+
+//filter the companies with two O's in their name---------------------------------------------------------------------------------
+
+let itCompanie = [];
+itCompanie.push("Facebook","Google","Microsoft","Apple","Apple","Oracle","Amazon");
+
+function hasOO(array){
+let regex = /o{2}/;
+let filtered = [];
+for(let company of array){
+    let result = regex.test(company) ;
+    if (result){
+        filtered.push(company);
+    }
+}
+console.log(filtered);
+}
+
+hasOO(itCompanie);
+
+//Sort the companies names---------------------------------------------------------------------------------
+
+
+
+
